5e152b5b784f71ae37159906ef25779e4e3dad1c
Jakub Ďuraš <jakub@duras.me>
2019-05-17 02:50:11 +0200

    Remove unused import

--- src/test/java/me/duras/piatkovemocky/LogikaTest.java
+++ src/test/java/me/duras/piatkovemocky/LogikaTest.java
@@ -3,7 +3,6 @@ package me.duras.piatkovemocky;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
